e0559e17fd2e0f91ecddcd3bf68d4f207a5857d5
Geekdojo <bryce@geekdojo.com>
2026-07-07 16:17:32 -0700

    os: bump control-plane pin to 2026.07.1-dev.38
    Carries the app catalog (embedded catalog tiles + /app-catalog UI) into the
    image for bench testing.

--- package/rasputin-api/rasputin-api.mk
+++ package/rasputin-api/rasputin-api.mk
@@ -10,7 +10,7 @@
 # release's published *.hash assets.
 ################################################################################
 
-RASPUTIN_API_VERSION = 2026.07.1-dev.37
+RASPUTIN_API_VERSION = 2026.07.1-dev.38
 RASPUTIN_API_SITE = https://github.com/geekdojo/rasputin-control-plane/releases/download/v$(RASPUTIN_API_VERSION)
 
 ifeq ($(BR2_aarch64),y)
